d22d43a3f393ce893b63212b805f450be97eadea
JonDev22 <jonnydeveloper22@gmail.com>
2022-02-17 18:28:36 +0100

    Initialized the last packages and the package.json.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import logger from './middleware/logger';
 
 const app = express();
 const port = 3000;
+
+app.get('/', logger, (req, res) => {
+    res.send('Endpoint');
+});
 
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
